b7bf9864d2b879b91c8f5ff908d8a6892bc9b0de
Nicole <nicolejoan9@yahoo.com>
2023-08-03 17:18:51 -0400

    added a for each to loop through the divs and apply styling for current, past or present time

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,56 +1,45 @@
 var today = dayjs();
 
-var currentTime = today.format("hh:mm a");
-console.log(currentTime);
+var currentTime = today.format("H");
 
 var currentDay = $("#currentDay").text(
   today.format(" dddd MMMM D YYYY" + " " + "hh:mm a")
 );
 
-workDaySchedule = [
-  { time: "9 am", event: "" },
-  { time: "10 am ", event: "" },
-  { time: "11 am", event: "" },
-  { time: "12 pm", event: "" },
-  { time: "1 pm", event: "" },
-  { time: "2 pm", event: "" },
-  { time: "3 pm", event: "" },
-  { time: "4 pm", event: "" },
-  { time: "5 pm", event: "" },
-];
+function timeColor() {
+  $(".time-block").each(function () {
+    var hour = parseInt(this.id);
+    $(this).toggleClass("past", hour < currentTime);
+    $(this).toggleClass("present", hour == currentTime);
+    $(this).toggleClass("future", hour > currentTime);
+ });
+}
+
+timeColor();
+
+
+
+$(".saveBtn").on("click", function () {
+  var event = $(this).closest(".description").val();
+  var hour = $(this).closest(".time-block").attr("id");
+  localStorage.setItem(hour, event);
+});
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-for (let i = 0; i < workDaySchedule.length; i++) {
-  var timeBlockTime = workDaySchedule[i];
-  // timeBlockTime =
-  if (currentTime === timeBlockTime) {
-    $("#hour").addClass("row time-block times present");
-  } else if (currentTime < timeBlockTime) {
-    $("#hour").addClass("row time-block times past");
-  } else {
-    $("#hour").addClass("row time-block times future");
-  }
-};
+//
+//  make a loop to select each item with the a common class using jquery, you can take that id and compare the number to the current hour
 
- 
- 
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
 // function? How can DOM traversal be used to get the "hour-x" id of the
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
+
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
-//
+
